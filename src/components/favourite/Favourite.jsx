f08373922b823562e13693f43f58e682836fb56a
Ajay Gangisetti <ajay.g415@gmail.com>
2026-01-17 18:19:44 +0530

    added fav cards

--- src/components/favourite/Favourite.jsx
+++ src/components/favourite/Favourite.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import FundCard from "../common/FundCard";
+import { useSelector } from "react-redux"; 
 
-const Favourite = () => {
+const Favoutite = () => {
+    const { funds } = useSelector((state) => state.mf.userData);
+    const favFUnds = funds.filter(fund => fund.isFavorite);
     return (
-        <section className="favourite-section">
-            <h1>Welcome to the Favourite</h1>
+        <section className="favoutite-section">
+            {favFUnds && favFUnds.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                    {favFUnds.map((fund) => (
+                        <FundCard key={fund.code} fund={fund} />
+                    ))}
+                </div>
+            ) : (
+                <p className="text-gray-500 mt-4">No funds to display.</p>
+            )}
         </section>
     );
-};
-export default Favourite;
+}
+export default Favoutite;
